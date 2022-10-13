f455fecde0c015b7cb7db9008cd5e423d11de2e4
Alfred Idoko <alfredidoko@yahoo.com>
2022-10-13 11:46:26 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Programming is like building a multilingual puzzle
- * Return: 0 if exited
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building a multi	    lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
