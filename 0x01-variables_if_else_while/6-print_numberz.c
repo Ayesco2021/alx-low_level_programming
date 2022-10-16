3ab23041e5e5b6a073a9d913fcbb58e0d7a32b56
Alfred Idoko <alfredidoko@yahoo.com>
2022-10-16 03:13:36 -0700

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main: prints all single digit numbers of base 10
+* main: print 0123456789 using putchar
 * Return: Always 0 (success)
 */
 
@@ -10,7 +10,7 @@ int main(void)
 {
 	int n = '0';
 
-	while (n < '9')
+	while (n <= '9')
 	{
 		putchar(n);
 		n++;
