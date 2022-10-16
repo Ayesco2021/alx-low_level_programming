c5f5a6e4e2378443da94176510d8f8e47693f01a
Alfred Idoko <alfredidoko@yahoo.com>
2022-10-16 10:50:43 -0700

    task11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - print 00 to 99 with no duplicate digits
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
  *Return: Always 0 (Success)
  */
 
@@ -10,10 +10,10 @@ int main(void)
 	int ones;
 	int tens;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
 	{
-		for (ones = (tens + 1); ones <= '9'; ones++)
-			putchar(tens);
+		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
+		{	putchar(tens);
 			putchar(ones);
 
 			if (tens != '8' || ones != '9') /*print commas*/
@@ -21,3 +21,8 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
