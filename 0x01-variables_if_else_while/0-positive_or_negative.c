04e59311551128fcd412a714ff6e9cf1c6a7ff50
Alfred Idoko <alfredidoko@yahoo.com>
2022-10-16 00:01:01 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,8 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
@@ -27,6 +25,3 @@ int main(void)
 	}
 	return (0);
 }
-
-
-
